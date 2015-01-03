9cb850d666472808b98ada463c0fa97bf9941571
Keith <keithlang@yahoo.com>
2015-01-03 17:31:15 +0000

    created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dasboard]
   def home
   	if current_user
   		redirect_to books_path
   	end
+  end
+
+  def dashboard
+  	@books = current_user.books
   end
 end
